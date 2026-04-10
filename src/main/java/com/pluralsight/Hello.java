64f4266729c69deb6515847cd21cba71f3d3d577
dubem <dubemdamah@gmail.com>
2026-04-10 10:02:19 -0400

    Initial commit

---
+++ src/main/java/com/pluralsight/Hello.java
@@ -0,0 +1,22 @@
+package com.pluralsight;
+
+public class Hello {
+    public static void main(String[] args) {
+        sayGoodbye();
+        sayHello();
+        sayGoodmorning();
+
+
+    }
+
+    public static void sayHello() {
+        System.out.println("Hello World!");
+    }
+    public static void sayGoodbye() {
+        System.out.println("Goodbye!");
+    }
+    public static void sayGoodmorning() {
+        System.out.println("Good morning!");
+    }
+
+}
